7e726b075feeba20dc9d2bd79047f604f1781453
Mihaela Cocieru <70962062+mihaelac01@users.noreply.github.com>
2021-10-14 11:52:22 +0300

    Add files via upload

---
+++ probl.3.py
@@ -0,0 +1,7 @@
+#modelul din manual
+print("Ce nume si prenume ai? ")
+nume=input()
+print("Ce iti doresti sa devii in viitor?")
+dorinta=input()
+print("Te numesti", nume)
+print("Doresti sa devii", dorinta)
